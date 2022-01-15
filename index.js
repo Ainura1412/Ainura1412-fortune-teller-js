5ed86f0100e020dcc5c31b64aec84b6096e31c31
Ainura <abekbolsunova@gmail.com>
2022-01-15 22:05:50 +0600

    Added first project

---
+++ index.js
@@ -0,0 +1,6 @@
+
+let numOfKids = 4;
+let partnerName = "Adil ";
+let geoLocation = "Oslo ";
+jobTitle = "Web developer";
+console.log("You will be a " + jobTitle + "in " + geoLocation + ','+ 'and married to ' + partnerName + ' with ' + numOfKids + ' kids' +'.');
